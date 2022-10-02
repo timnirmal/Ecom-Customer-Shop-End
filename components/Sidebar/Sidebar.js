62f4472ede4766ef2c8c4e8f13b7ca393f877bc4
timnirmal <timnirmal@gmail.com>
2022-10-03 01:36:32 +0530

    Customer Segmentation

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -341,7 +341,7 @@ export default function Sidebar() {
 
                         <ul className="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
                             <li className="items-center">
-                                <Link href="/auth/login">
+                                <Link href="/admin/customersegmentation">
                                     <a
                                         href="#pablo"
                                         className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
